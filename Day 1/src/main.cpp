221ae91ef8b2299fb73c7af8ac5c7dd53985e578
Anwesh Behera <anweshbehera09@gmail.com>
2025-11-09 15:09:01 +0530

    listing files in a directory

--- Day 1/src/main.cpp
+++ Day 1/src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <filesystem>
 namespace fs = std::filesystem;
-
 int main() {
     std::cout << "File Explorer Started\n";
     for (const auto &entry : fs::directory_iterator(fs::current_path()))
